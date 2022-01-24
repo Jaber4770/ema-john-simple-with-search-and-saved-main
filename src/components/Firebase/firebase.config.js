86be9974231342f1fbb698713f389fb2a2dbb58c
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-25 00:27:20 +0600

    firebase authentication task started

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD80iBEr2pgsQ5uRb6-eVsV2zFETDPX1ak",
+    authDomain: "ema-john-simple-bbba5.firebaseapp.com",
+    projectId: "ema-john-simple-bbba5",
+    storageBucket: "ema-john-simple-bbba5.appspot.com",
+    messagingSenderId: "53820912101",
+    appId: "1:53820912101:web:e36a0614ad439602983f0c"
+};
+
+export default firebaseConfig;
